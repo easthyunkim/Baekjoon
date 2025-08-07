60d2d5867ffa9b3f67a29d635ca55b000a221a71
Easthyun_Kim <donghyun070102@gmail.com>
2025-08-07 12:29:53 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,17 @@
+N = int(input())
+for i in range(N):
+    S = input()
+    if S == "Algorithm" :
+        print('204')
+    elif S == "DataAnalysis" :
+        print('207')
+    elif S == "ArtificialIntelligence" :
+        print('302')
+    elif S == "CyberSecurity" :
+        print('B101')   
+    elif S == "Network" :
+        print('303')
+    elif S == "Startup" :
+        print('501')
+    elif S == "TestStrategy" :
+        print('105')
