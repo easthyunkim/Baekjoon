e17d22877a8d03f25e5db03f57a19740e494d7f9
Easthyun_Kim <donghyun070102@gmail.com>
2025-08-13 23:22:02 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.py
+++ Python/백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.py
@@ -1,33 +1,15 @@
-import sys
-input = sys.stdin.readline
-N = int(input())
-if N == 1 : 
-    print(11)
-    print("A B C D E F G H J L M")
-elif N == 2 :
-    print(9)
-    print("A C E F G H I L M")
-elif N == 3 :
-    print(9)
-    print("A C E F G H I L M")
-elif N == 4 :
-    print(9)
-    print("A B C E F G H L M")
-elif N == 5 :
-    print(8)
-    print("A C E F G H L M")
-elif N == 6 :
-    print(8)
-    print("A C E F G H L M")
-elif N == 7 :
-    print(8)
-    print("A C E F G H L M")
-elif N == 8 :
-    print(8)
-    print("A C E F G H L M")
-elif N == 9 :
-    print(8)
-    print("A C E F G H L M")
-elif N == 10 :
-    print(8)
-    print("A B C F G H L M")
+A = [
+    [11, "A B C D E F G H J L M"],
+    [9, "A C E F G H I L M"],
+    [9, "A C E F G H I L M"],
+    [9, "A B C E F G H L M"],
+    [8, "A C E F G H L M"],
+    [8, "A C E F G H L M"],
+    [8, "A C E F G H L M"],
+    [8, "A C E F G H L M"],
+    [8, "A C E F G H L M"],
+    [8, "A B C F G H L M"]
+]
+N = int(input()) - 1
+print(A[N][0])
+print(A[N][1])
