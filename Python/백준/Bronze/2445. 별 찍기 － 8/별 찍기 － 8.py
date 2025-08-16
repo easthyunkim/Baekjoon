3be789e8479d4d05e994efda391254659b2e2e46
Easthyun_Kim <donghyun070102@gmail.com>
2025-08-16 18:16:33 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+for i in range(1, N+1):
+    print("*"*i+" "*2*(N-i)+"*"*i)
+for i in range(1, N):
+    print("*"*(N-i)+" "*2*i+"*"*(N-i))
