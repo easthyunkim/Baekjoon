c75c936733aa915e3f379631891a10883953d345
Easthyun_Kim <donghyun070102@gmail.com>
2025-09-05 22:22:32 +0900

    [Bronze V] Title: 찾아오시는 길, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/34217. 찾아오시는 길/찾아오시는 길.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+A, B = map(int, input().split())
+C, D = map(int, input().split())
+if A+C < B+D:
+    print('Hanyang Univ.')
+elif A+C > B+D:
+    print('Yongdap')
+else:
+    print('Either')
