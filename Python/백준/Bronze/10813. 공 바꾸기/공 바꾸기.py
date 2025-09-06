f7d16a9764f2fa67ac07abbb98bef44137a9afe7
Easthyun_Kim <donghyun070102@gmail.com>
2025-09-06 21:24:37 +0900

    [Bronze II] Title: 공 바꾸기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+basket = list(range(1, N+1))
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    basket[i-1], basket[j-1] = basket[j-1], basket[i-1]
+
+for x in basket:
+    print(x, end=' ')
