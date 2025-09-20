ceb16b8d7fa335b28979a6b3d1df37886cf8f9f2
Easthyun_Kim <donghyun070102@gmail.com>
2025-09-20 10:08:36 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.py
@@ -0,0 +1,11 @@
+for _ in range(15):
+    P = list(input().split())
+    if 'w' in P:
+        print('chunbae')
+        break
+    elif 'b' in P:
+        print('nabi')
+        break
+    elif 'g' in P:
+        print('yeongcheol')
+        break
