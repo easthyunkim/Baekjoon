f0d9a70f8e24cdeffad1e7301bd6984a180c2b7d
Easthyun_Kim <donghyun070102@gmail.com>
2025-09-21 12:42:57 +0900

    [Bronze IV] Title: 준영이의 등급, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/30008. 준영이의 등급/준영이의 등급.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+# 입력 받기
+N, K = map(int, input().split())
+G = list(map(int, input().split()))
+result = []
+
+#반복문으로 결과값 저장하기
+for i in G:
+    P = (i*100)//N
+    if 0 <= P <= 4:
+        result.append(1)
+    elif 4 < P <= 11:
+        result.append(2)
+    elif 11 < P <= 23:
+        result.append(3)
+    elif 23 < P <= 40:
+        result.append(4)
+    elif 40 < P <= 60:
+        result.append(5)
+    elif 60 < P <= 77:
+        result.append(6)
+    elif 77 < P <= 89:
+        result.append(7)
+    elif 89 < P <= 96:
+        result.append(8)
+    elif 96 < P <= 100:
+        result.append(9)
+
+#출력하기
+print(' '.join(map(str, result)))
