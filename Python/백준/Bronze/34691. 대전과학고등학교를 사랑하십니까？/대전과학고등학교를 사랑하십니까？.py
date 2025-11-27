ebd36c64a9ae0087361c288677f30b4dd7a9cbd6
Easthyun_Kim <donghyun070102@gmail.com>
2025-11-27 13:24:17 +0900

    [Bronze V] Title: 대전과학고등학교를 사랑하십니까?, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/34691. 대전과학고등학교를 사랑하십니까？/대전과학고등학교를 사랑하십니까？.py
@@ -0,0 +1,10 @@
+while True:
+    N = str(input())
+    if N == 'animal':
+        print('Panthera tigris')
+    elif N == 'tree':
+        print('Pinus densiflora')
+    elif N == 'flower':
+        print('Forsythia koreana')
+    elif N == 'end':
+        break
