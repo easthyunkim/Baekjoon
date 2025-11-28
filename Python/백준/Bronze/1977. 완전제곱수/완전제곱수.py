9f8da70ab3a86f69bee96860f9b16a1ce252ef3b
Easthyun_Kim <donghyun070102@gmail.com>
2025-11-28 10:37:44 +0900

    [Bronze II] Title: 완전제곱수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/1977. 완전제곱수/완전제곱수.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+M = int(input())
+N = int(input())
+sum = 0
+min = 0
+
+for i in range(1, 101):
+    if M <= i*i and N >= i*i:
+        if sum == 0:
+            min = i*i
+        sum += i*i
+
+if sum == 0:
+    print(-1)
+else:
+    print(sum)
+    print(min)
