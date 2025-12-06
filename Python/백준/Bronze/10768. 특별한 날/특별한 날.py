755c161f30e8d73bbff86348051bbae3221d3148
Easthyun_Kim <donghyun070102@gmail.com>
2025-12-06 13:43:00 +0900

    [Bronze IV] Title: 특별한 날, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10768. 특별한 날/특별한 날.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+A = int(input())
+B = int(input())
+
+if A == 1:
+    print('Before')
+elif A == 2:
+    if B < 18:
+        print('Before')
+    elif B == 18:
+        print('Special')
+    else:
+        print('After')
+else:
+    print('After')
