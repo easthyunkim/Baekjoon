23a3dee9a1ea19a7d1457ee058836675761cdef2
Easthyun_Kim <donghyun070102@gmail.com>
2025-12-08 09:57:36 +0900

    [Bronze III] Title: 페르시아의 왕들, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10599. 페르시아의 왕들/페르시아의 왕들.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    a, b, c, d = map(int, input().split())
+
+    if a == b == c == d == 0:
+        break
+    else:
+        print(c-b, d-a)
