b9aad5315d7bd8f930d492324bd223ab719b2a6a
Easthyun_Kim <donghyun070102@gmail.com>
2025-12-16 16:12:15 +0900

    [Bronze III] Title: 사탕 선생 고창영, Time: 468 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2547. 사탕 선생 고창영/사탕 선생 고창영.py
@@ -0,0 +1,11 @@
+T = int(input())
+for i in range(T):
+    blank = input()
+    N = int(input())
+    candy = 0
+    for j in range(N):
+        candy += int(input())
+    if candy % N == 0:
+        print('YES')
+    else:
+        print('NO')
