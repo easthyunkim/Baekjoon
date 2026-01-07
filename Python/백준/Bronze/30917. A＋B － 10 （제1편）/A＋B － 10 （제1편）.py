a2bf7dfd4f552a528e38fa0182d1f2151ace61c1
Easthyun_Kim <donghyun070102@gmail.com>
2026-01-07 16:39:30 +0900

    [Bronze III] Title: A+B - 10 (제1편), Time: 4 ms, Memory: 13676 KB -BaekjoonHub

---
+++ Python/백준/Bronze/30917. A＋B － 10 （제1편）/A＋B － 10 （제1편）.py
@@ -0,0 +1,15 @@
+for a in range(1, 10):
+    print("? A", a, flush=True)
+    resp = int(input())
+    if resp == 1:
+        A = a
+        break
+
+for b in range(1, 10):
+    print('? B', b, flush = True)
+    resp = int(input())
+    if resp == 1:
+        B = b
+        break
+
+print('!', A+B, flush = True)
