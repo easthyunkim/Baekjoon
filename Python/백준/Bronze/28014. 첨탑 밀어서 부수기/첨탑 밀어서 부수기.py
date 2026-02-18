7b84491434ff23e2629bc2a6d89e740d2afdc157
Easthyun_Kim <donghyun070102@gmail.com>
2026-02-18 21:39:52 +0900

    [Bronze III] Title: 첨탑 밀어서 부수기, Time: 1920 ms, Memory: 542152 KB -BaekjoonHub

---
+++ Python/백준/Bronze/28014. 첨탑 밀어서 부수기/첨탑 밀어서 부수기.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+cnt = 1
+
+for i in range(n-1):
+    if arr[i] <= arr[i+1]:
+        cnt += 1
+
+print(cnt)
