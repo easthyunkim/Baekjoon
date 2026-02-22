849802d4212434fe470cc673578773d424a222f5
Easthyun_Kim <donghyun070102@gmail.com>
2026-02-22 17:44:21 +0900

    [Silver II] Title: DFS와 BFS, Time: 56 ms, Memory: 35712 KB -BaekjoonHub

--- Python/백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ Python/백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,46 +1,39 @@
-#학습을 위해 참고한 블로그
-#https://velog.io/@falling_star3/백준Python-1260번-DFS와BFS
-
-import sys
 from collections import deque
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
 
-#입력 받기
-n, m ,v = map(int, input().split())
+n, m, r = map(int, input().split())
 gph = [[] for _ in range(n+1)]
 visited = [False]*(n+1)
 
-#DFS 정의하기
-def dfs(n):
-    visited[n] = True
-    print(n, end=' ')
-    for i in gph[n]:
-        if not visited[i]:
-            dfs(i)
-
-#bfs 정의하기
-def bfs(n):
-    visited[n] = True
-    queue = deque([n])
+def bfs(gph, v, visited):
+    queue = deque([v])
+    visited[v] = True
     while queue:
         v = queue.popleft()
         print(v, end=' ')
         for i in gph[v]:
             if not visited[i]:
                 queue.append(i)
-                visited[i]=True
+                visited[i] = True
 
-#인접리스트 만들기
+def dfs(gph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in gph[v]:
+        if not visited[i]: 
+            dfs(gph, i, visited)
+
 for i in range(m):
     a, b = map(int, input().split())
     gph[a].append(b)
     gph[b].append(a)
 
-#인접리스트 정렬
-for i in range(1, n+1):
+for i in range(n+1):
     gph[i].sort()
 
-#출력, 체크리스트 초기화
-dfs(v)
+dfs(gph, r, visited)
 visited = [False]*(n+1)
 print()
-bfs(v)
+bfs(gph, r, visited)
