e1f4d8df5e92a97a54e76248a1e92e817512d4f7
Easthyun_Kim <donghyun070102@gmail.com>
2026-02-22 17:31:30 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 396 ms, Memory: 61176 KB -BaekjoonHub

---
+++ Python/백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,35 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+n, m, r = map(int, input().split())
+gph = [[] for _ in range(n+1)]
+visited = [0]*(n+1)
+cnt = 1
+
+def bfs(gph, v, visited):
+    global cnt
+    queue = deque([v])
+    visited[v] = cnt #방문 시 순서 세기
+    while queue:
+        v = queue.popleft()
+        for i in gph[v]: #인접 정점 중
+            if not visited[i]: #방문 안한 정점
+                queue.append(i)
+                cnt += 1
+                visited[i] = cnt
+            
+
+for i in range(m):
+    a, b = map(int, input().split())
+    gph[a].append(b)
+    gph[b].append(a) #양방향 간선
+
+for i in range(n+1):
+    gph[i].sort() #오름차순 정렬
+
+bfs(gph, r, visited)
+
+for i in range(1, n+1):
+    print(visited[i])
