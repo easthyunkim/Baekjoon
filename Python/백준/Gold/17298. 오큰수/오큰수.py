a30994b2f8a096f3e64c0d77665c121ebddd602f
kdh712 <donghyun070102@gmail.com>
2026-02-26 21:10:29 +0900

    [Gold IV] Title: 오큰수, Time: 1036 ms, Memory: 136860 KB -BaekjoonHub

---
+++ Python/백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+A = list(map(int, input().split()))
+ans = [-1]*N
+stack = [0]
+
+for i in range(N):
+    while stack and A[stack[-1]] < A[i]:
+        ans[stack.pop()] = A[i]
+    stack.append(i)
+
+print(*ans)
