44d2742b89e827941e4f694e1b5230c268c0a434
kdh712 <donghyun070102@gmail.com>
2026-03-03 08:04:54 +0900

    [Bronze II] Title: Words, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ PyPy3/백준/Bronze/4072. Words/Words.py
@@ -0,0 +1,9 @@
+while 1:
+    arr = list(input().split())
+    if arr[0] == '#':
+        break
+    for i in range(len(arr)):
+        if (i == len(arr)-1):
+            print(arr[i][::-1])
+            break
+        print(arr[i][::-1], end=' ')
