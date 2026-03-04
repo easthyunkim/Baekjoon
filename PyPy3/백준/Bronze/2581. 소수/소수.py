81688d9134b757e4ceb92bcac00594448cde47e0
kdh712 <donghyun070102@gmail.com>
2026-03-04 11:43:33 +0900

    [Bronze II] Title: 소수, Time: 172 ms, Memory: 109544 KB -BaekjoonHub

---
+++ PyPy3/백준/Bronze/2581. 소수/소수.py
@@ -0,0 +1,19 @@
+m = int(input())
+n = int(input())
+lst = []
+
+for i in range(m, n+1):
+    cnt = 0
+    if i > 1:
+        for j in range(2, i):
+            if i%j == 0:
+                cnt += 1
+                break
+        if cnt == 0:
+            lst.append(i)
+
+if len(lst) > 0:
+    print(sum(lst))
+    print(min(lst))
+else:
+    print(-1)
